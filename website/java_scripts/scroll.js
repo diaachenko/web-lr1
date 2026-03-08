5b6f8079431e778f0cad260d4bc677a25bf3295f
Diaachenko <dacenkonazar3@gmail.com>
2026-03-08 15:21:03 +0200

    added scroll script

---
+++ website/java_scripts/scroll.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+	const siteLogo = document.getElementById('logo');
+
+    if (logo) {
+        logo.addEventListener('click', (event) => {
+            event.preventDefault(); // Prevent the default jump to #top which is instant
+
+            window.scrollTo({
+                top: 0,            // Scroll to the very top of the page
+                behavior: 'smooth' // Make the scrolling animation smooth
+            });
+        });
+    }
