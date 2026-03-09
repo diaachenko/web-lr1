45af59bed0e3e403d420f54be96ab5b5b21d3cf4
Diaachenko <dacenkonazar3@gmail.com>
2026-03-09 12:52:49 +0200

    finish js script for reading file

--- website/java_scripts/read_cards.js
+++ website/java_scripts/read_cards.js
@@ -1,7 +1,37 @@
 fetch('json/suites.json')
 	.then(response => response.json())
 	.then(data => {
+		const Container = document.getElementById('cards');
+
+		if (data.length() > 0) {
+			let i = 0;
+
+			do {
+				let card = document.createElement('div');
+				card.className = "property-card";
+
+				let featureList = "";
+				for (let j = 0; j < data[i].features.length; j++) {
+					featureList += `<li>${data[i].features[j]}</li>`;
+				}
 
 
+				card.innerHTML = `
+					<img scr="${data[i].image}" alt="${data[i].alt}">
+					<h3>${data[i].title}</h3>
+					<h5>${data[i].price}</h5>
+					<details>
+						<summary>Details</summary>
+						<p>${data[i].description}</p>
+						<ul>${featureList}</ul>
+					</details><br>
+					<button>Book</button>
+				`;
+
+				Container.appendChild(card);
+
+				i++;
+			} while (i < data.length());
+		}
 	})
 	.catch(error => console.error('Error:', error));
